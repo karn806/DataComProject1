baec175473d9fa6028df12b1783a55d6f4e49a84
Karn <nattakarnklongyut@Nattakarns-MacBook-Pro.local>
2016-01-22 00:00:35 +0700

    22-01-16

--- testNew.py
+++ testNew.py
@@ -31,41 +31,73 @@ sock.send(request)
 
 
 
-def getContentLength(wholeData):
-	index = data.find("Content-Length: ")
+def getHeader(socket_input):
+	content = ''
+	while True:
+		chunk = sock.recv(1024)
+		content+=chunk
+		if "\r\n\r\n" in content:
+			findrnrnIndex = content.find('\r\n\r\n')
+			header = content[:findrnrnIndex+4] ## start to end of header
+			therest = content[findrnrnIndex+4:] ## the firse few chunk of header
+			break
+	return header, therest #header, therest
+# print getHeader(sock)
+
+def getContentLength(header):
+
+	index = header.find("Content-Length: ")
 	# print index
-	ContentLength = data[index:]
+	ContentLength = header[index:]
 	#print ContentLength
 	indexColon = ContentLength.find(":")
 	numContentLength = ContentLength[indexColon+2:]
 	# print numContentLength
 	findBackrn = numContentLength.find("\r\n")
 	pureContentLength = numContentLength[:findBackrn]
-	print pureContentLength
 
-def getRidOfHeader(Data):
-	if "\r\n\r\n" in Data:
-		findrnrnIndex = Data.find('\r\n\r\n')
-		sliceHeader = Data[findrnrnIndex+4:]
-	print sliceHeader
+	return pureContentLength
+# print getContentLength(sock)
+
+def get_body(socket_input):
+	header, body = getHeader(socket_input) ## get the rest from tuple(header, therest)
+	# print header, '----header'
+	# print body, '-------body'
+	ContentLength = getContentLength(header) ## type == str
+	# print ContentLength, '-------------cl'
+	# print len(body), '-------len(body)'
+
+	while True:
+		chunk = sock.recv(1024)
+		body += chunk
+		if len(body) == int(ContentLength):
+			print 'download done'
+			break
+	return body
+# print 'hi'
+print get_body(sock)
 
 
-count = 0
-# with open('keepAllData.txt', 'w') as f:
-while True:
-	data = sock.recv(1024)
-	if "Content-Length: " and "\r\n\r\n" in data:
-		while count<1:
-			ContentLength = getContentLength(data)
-			count+=1
-	getBody = getRidOfHeader(data)
-	if ContentLength == 
-	# # write data into txt file
-	# with open('keepAllData.txt', 'w') as f:
-	# 	f.write(data)
 
-	sock.close
-	break
+
+
+
+# count = 0
+# # with open('keepAllData.txt', 'w') as f:
+# while True:
+# 	data = sock.recv(1024)
+# 	if "Content-Length: " and "\r\n\r\n" in data:
+# 		while count<1:
+# 			ContentLength = getContentLength(data)
+# 			count+=1
+# 	# getBody = getRidOfHeader(data)
+# 	if ContentLength == 
+# 	# # write data into txt file
+# 	# with open('keepAllData.txt', 'w') as f:
+# 	# 	f.write(data)
+
+# 	sock.close
+# 	break
 
 
 
