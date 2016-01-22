34ee4aae1d167b8c79c93170a571b15ed7f8d50f
Karn <nattakarnklongyut@Nattakarns-MacBook-Pro.local>
2016-01-22 21:45:22 +0700

    22-01-16_9:44pm

--- findLength.py
+++ findLength.py
@@ -6,7 +6,7 @@ def mkDownloadRequest(serv, objName):
 
 #print "{!r}".format(mkDownloadRequest("intranet.mahidol", "/"))
 
-servName = "intranet.mahidol"
+servName = "www.google.com"
 port = 80
 ## create an empty socket
 sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
@@ -19,31 +19,31 @@ sock.send(request)
 while True:
 	data = sock.recv(1024)
 	print data
-	if "\r\n\r\n" in data:
-		index = data.find("Content-Length: ")
-		# print index
-		ContentLength = data[index:]
-		#print ContentLength
-		indexColon = ContentLength.find(":")
-		numContentLength = ContentLength[indexColon+2:]
-		# print numContentLength
-		findBackrn = numContentLength.find("\r\n")
-		pureContentLength = numContentLength[:findBackrn] #now pureContentLength is str type
-		print "=========start========="
-		# print pureContentLength
-		# print "===========End========"
-		indexOfrn = data.find("\r\n\r\n")
-		body = data[indexOfrn+4:]
+	# if "\r\n\r\n" in data:
+	# 	index = data.find("Content-Length: ")
+	# 	# print index
+	# 	ContentLength = data[index:]
+	# 	#print ContentLength
+	# 	indexColon = ContentLength.find(":")
+	# 	numContentLength = ContentLength[indexColon+2:]
+	# 	# print numContentLength
+	# 	findBackrn = numContentLength.find("\r\n")
+	# 	pureContentLength = numContentLength[:findBackrn] #now pureContentLength is str type
+	# 	print "=========start========="
+	# 	# print pureContentLength
+	# 	# print "===========End========"
+	# 	indexOfrn = data.find("\r\n\r\n")
+	# 	body = data[indexOfrn+4:]
 		# print body
 		# print "------------"
 		# print len(data), '-----len data'
-		print len(body),'---------len body'
-		print pureContentLength
+		# print len(body),'---------len body'
+		# print pureContentLength
 
-		if int(pureContentLength) == len(body):
-			print "download done"
-		sock.close()
-		break
+		# if int(pureContentLength) == len(body):
+		# 	print "download done"
+		# sock.close()
+		# break
 	# if len(data)==0:
 	sock.close()
 	break
